c951a720e24d23a040dfc08b77ffd44f3f65654f
BerkayKaranfil <berkaykaranfil97@gmail.com>
2022-12-19 17:31:08 +0300

    1.05

--- lib/modules/detail/detail_screen.dart
+++ lib/modules/detail/detail_screen.dart
@@ -11,8 +11,61 @@ class DetailScreen extends GetView<DetailController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      //appBar: AppBar(),
-      body: Column(
+        //appBar: AppBar(),
+        body: Padding(
+      padding: EdgeInsets.all(5.h),
+      child: Column(
+        children: [
+          Card(
+            elevation: 40,
+            color: Colors.white,
+            margin: EdgeInsets.only(bottom: 35.h),
+            child: Padding(
+              padding: EdgeInsets.all(3.h),
+              child: SizedBox(
+                height: 40.h,
+                width: 80.w,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(
+                      "Your BMI is",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(fontSize: 5.h),
+                    ),
+                    SizedBox(
+                      height: 5.h,
+                    ),
+                    Text(
+                      controller.bmi!,
+                      style: TextStyle(fontSize: 5.h),
+                    ),
+                    SizedBox(
+                      height: 5.h,
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(left: 5.h),
+                      height: 3.5.h,
+                      width: 50.w,
+                      child: Text(
+                        "${controller.bmiStatus}",
+                        style: TextStyle(fontSize: 3.h),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          ElevatedButton(
+              onPressed: () {
+                Get.back();
+              },
+              child: Text("Yeniden Hesapla"))
+        ],
+      ),
+    )
+        /* Column(
         children: [
           SizedBox(
             height: 10.h,
@@ -30,7 +83,7 @@ class DetailScreen extends GetView<DetailController> {
             Get.back();
           }, child: Text("Yeniden Hesapla"))
         ],
-      ),
-    );
+      ), */
+        );
   }
 }
